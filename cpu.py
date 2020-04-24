3b5777da554f4dbd8433c98ba0dff16e7990d58d
Nattajohn Rojanasupya <n.rojanasupya@gmail.com>
2020-04-24 08:47:15 -0700

    adds CMP operation

--- cpu.py
+++ cpu.py
@@ -2,6 +2,9 @@
 
 import sys
 
+SP = 7
+
+# Instructions
 LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
@@ -11,9 +14,19 @@ PUSH = 0b01000101
 POP = 0b01000110
 CALL= 0b01010000
 RET = 0b00010001
+CMP = 0b10100111
+JMP = 0b01010100
+
+# Masks
 INSTRUCTION_MASK = 0b11000000
 ALU_MASK = 0b00100000
 AUTOINCREMENT_MASK = 0b00010000
+LGE_MASK = 0b00000111
+
+# CMP codes
+L = 0b100
+G = 0b10
+E = 0b1
 
 class CPU:
     """Main CPU class."""
@@ -22,13 +35,14 @@ class CPU:
         """Construct a new CPU."""
         self.ram = [0] * 256
         self.reg = [0] * 8
+        self.fl = 0b0000000
 
-        self.reg[7] = 0xf4
+        self.reg[SP] = 0xf4
 
         self.pc = 0
         self.running = True
 
-        self.dispatcher = {
+        self.branch_table = {
             LDI: self.LDI,
             PRN: self.PRN,
             HLT: self.HLT,
@@ -52,28 +66,50 @@ class CPU:
 
     def PUSH(self, call=False):
         # shift stack pointer down to the next empty slot
-        self.reg[7] -= 1
+        self.reg[SP] -= 1
 
         if call:
             # push the address of pc + 2 to the stack
-            self.ram_write(self.reg[7], self.pc + 2)
+            self.ram_write(self.reg[SP], self.pc + 2)
         else:
             # push the value at pc + 1 to the stack
             register_num = self.ram_read(self.pc + 1)
-            self.ram_write(self.reg[7], self.reg[register_num])
+            self.ram_write(self.reg[SP], self.reg[register_num])
 
     def POP(self, ret=False):
         register_num = self.ram_read(self.pc + 1)
 
         if ret:
             # set pc to the address stored at the top of the stack
-            self.pc = self.ram_read(self.reg[7])
+            self.pc = self.ram_read(self.reg[SP])
         else:
             # pop the value at the top of the stack to a specific register number
-            self.reg[register_num] = self.ram_read(self.reg[7])
+            self.reg[register_num] = self.ram_read(self.reg[SP])
 
         # shift stack pointer up to the next item on the stack
-        self.reg[7] += 1
+        self.reg[SP] += 1
+    
+    def ADD(self, reg_a, reg_b):
+        self.reg[reg_a] += self.reg[reg_b]
+    
+    def MUL(self, reg_a, reg_b):
+        self.reg[reg_a] *= self.reg[reg_b]
+    
+    def CMP(self, reg_a, reg_b):
+        if self.reg[reg_a] < self.reg[reg_b]:
+            self.fl |= L
+        else:
+            self.fl &= 0b011
+
+        if self.reg[reg_a] < self.reg[reg_b]:
+            self.fl |= G
+        else:
+            self.fl &= 0b101
+
+        if self.reg[reg_a] > self.reg[reg_b]:
+            self.fl |= E
+        else:
+            self.fl &= 0b110
     
     def CALL(self):
         # set the pc to the value stored in the register number
@@ -107,10 +143,8 @@ class CPU:
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-        if op == ADD:
-            self.reg[reg_a] += self.reg[reg_b]
-        elif op == MUL: 
-            self.reg[reg_a] *= self.reg[reg_b]
+        if op in self.branch_table:
+            self.branch_table[op](reg_a, reg_b)
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -148,7 +182,7 @@ class CPU:
                 register_b = self.ram_read(self.pc + 2)
                 self.alu(ir, register_a, register_b)
 
-            elif ir in self.dispatcher:
+            elif ir in self.branc_table:
                 self.dispatcher[ir]()
 
             else:
